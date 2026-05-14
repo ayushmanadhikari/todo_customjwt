b91673d9a9b6b3bfd5a75fdefa3c28524f0b70eb
Ayushman Adhikari <unixparadoxfb@gmail.com>
2026-05-14 10:33:09 +0545

    added customuser serializer and registered in admin.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from rest_framework import serializers
 
 # Create your models here.
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=50, blank=False, null=False, unique=True)
     email = models.EmailField(blank=True, null=True)
-    name = models.CharField(blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
-    REQUIRED_FIELDS = ['username']
     USERNAME_FIELD = 'username'
-    
+
     def __str__(self):
         return self.username
+
+
+# serializer for CustomUser model
+class CustomUserSerializer(serializers.Serializer):
+    class Meta:
+        models = CustomUser
+        fields = '__all__'
